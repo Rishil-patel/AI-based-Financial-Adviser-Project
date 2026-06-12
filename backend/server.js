a4623e401ac6787a0b4625c0422fe6f3fae9aca6
rishilpatelit23d1-ship-it <rishilpatel.it23d1@scet.ac.in>
2026-06-12 21:30:22 +0530

    update login and sign-up function with online database

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
 
-const { connectDB, sequelize } = require("./config/db");
+const { connectDB } = require("./config/db");
 const authRoutes = require("./routes/authRoutes");
 
 const app = express();
@@ -12,7 +12,7 @@ app.use(express.json());
 
 connectDB();
 
-sequelize.sync({ alter: true });
+// sequelize.sync({ alter: true });
 
 app.use("/api/auth", authRoutes);
 
@@ -25,3 +25,4 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on ${PORT}`);
 });
+
